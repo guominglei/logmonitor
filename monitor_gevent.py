15edecd0a46e4353d4ca789c8c36873e6c89f1b6
guominglei <rickguominglei@hotmail.com>
2017-01-10 15:12:50 +0800

    修改

--- monitor_gevent.py
+++ monitor_gevent.py
@@ -40,54 +40,48 @@ DNS_DICT = {
 
 class ProcessSingleFile(pyinotify.ProcessEvent):
 
-    def __init__(self, log, sender, tags, queue, *args, **kwargs):
+    def __init__(self, log, sender, tags, *args, **kwargs):
         if isinstance(log, FileObject):
             self.log = log
         else:
             self.log = FileObject(log)
         self.tags = tags
         self.sender = sender
-        self.msg_queue = queue
         super(ProcessSingleFile, self).__init__(*args, **kwargs)
 
     def process_IN_MODIFY(self, event):
         for line in self.log.read_line():
-            #self.sender.captureMessage(line, tags=self.tags)
-            self.msg_queue.put([self.sender, line, self.tags])
+            self.sender.captureMessage(line, tags=self.tags)
+            gevent.sleep(0.1)
 
 
 class SentryLog(object):
 
-    def __init__(self, dns_dict, server_name, server_ip, basedir="/opt/sports"):
+    def __init__(self, project, dns_config, server_name,
+                 server_ip, basedir="/opt/sports"):
 
-        if not os.path.exists(basedir):
+        if not dns_config:
             sys.exit()
 
-        if not dns_dict:
-            sys.exit()
-
-        self.dns_dict = dns_dict
+        self.project = project
+        self.dns_config = dns_config
         self.server_name = server_name
         self.server_ip = server_ip
-        self.basedir = basedir
-        self.sender_dict = {}
+        self.base_dir = basedir
 
-        self.tags_dict, self.logpath_dict, self.file_dict = self.__init_info()
-
+        self.__init_info()
         self.__init_sender()
+
         self.wm = pyinotify.WatchManager()
         self.notifier = pyinotify.Notifier(self.wm)
 
-        self.msg_queue = Queue()
-        self.RUN = True
-
     def __init_sender(self):
 
-        for project, project_config in self.dns_dict.items():
+        for project, project_config in self.dns_config.items():
 
             dns = project_config.get("dns")
             if dns:
-                self.sender_dict[project] = raven.Client(
+                self.sender = raven.Client(
                     dns, transport=GeventedHTTPTransport
                 )
 
@@ -95,38 +89,35 @@ class SentryLog(object):
         """
             根据dns配置，初始化日志文件信息
         """
-        tags_dict = {}
-        log_dict = {}
-        file_dict = {}
+
         log_template = "{}/{}/logs/{}.log"
 
-        for project, project_config in self.dns_dict.items():
+        for dns, config_logpath in self.dns_config.items():
 
-            config_log_path = project_config.get("log_path")
-            if not config_log_path:
-                log_path = log_template.format(self.basedir, project, project)
+            if not config_logpath:
+                log_path = log_template.format(
+                    self.basedir, self.project, self.project
+                )
             else:
-                log_path = config_log_path
+                log_path = config_logpath
 
             if os.path.exists(log_path):
                 tag = {
                     "name": self.server_name,
                     "ip": self.server_ip,
-                    "project": project,
+                    "project": self.project,
                     "path": log_path
                 }
                 # 标签
-                tags_dict[project] = tag
+                self.tags = tag
                 # 日志文件路径
-                log_dict[project] = log_path
+                self.log_path = log_path
                 # 日志文件
                 tail_f = self.__tail_log(log_path)
-                file_dict[project] = tail_f
+                self.log_file = tail_f
 
             else:
                 print "log_path: {} not find".format(log_path)
-
-        return tags_dict, log_dict, file_dict
 
     def __tail_log(self, log_path):
 
@@ -139,56 +130,55 @@ class SentryLog(object):
 
     def monitor_start(self):
 
-        for project in self.file_dict.keys():
-            log_path = self.logpath_dict.get(project)
-            log_file = self.file_dict.get(project)
-            sender = self.sender_dict.get(project)
-            tags = self.tags_dict.get(project)
-            self.wm.add_watch(
-                log_path, pyinotify.IN_MODIFY,
-                proc_fun=ProcessSingleFile(log_file, sender, tags),
-                rec=False, auto_add=False, do_glob=False,
-                exclude_filter=lambda path: False)
+        self.wm.add_watch(
+            self.log_path, pyinotify.IN_MODIFY,
+            proc_fun=ProcessSingleFile(self.log_file, self.sender, self.tags),
+            rec=False, auto_add=False, do_glob=False,
+            exclude_filter=lambda path: False)
 
         self.notifier.loop()
 
-        gevent.spawn(self.sender_worker)
-
     def monitor_stop(self):
         self.notifier.stop()
-        self.RUN = False
 
     def monitor_restart(self):
         self.notifier.stop()
         self.notifier.loop()
 
-    def sender_worker(self):
-
-        msg = self.msg_queue.get()
-        while self.RUN:
-            if msg:
-                sender, msg, tags = msg
-                sender.captureMessage(msg, tags=tags)
-            else:
-                gevent.sleep(1)
-
 
 def main():
 
+    log_list = []
     if len(sys.argv) == 4:
         server_name, server_ip, basedir = sys.argv[1:]
-        sentry_log = SentryLog(DNS_DICT, server_name, server_ip, basedir)
+        for project, dns_config in DNS_DICT.items():
+            sentry_log = SentryLog(
+                project, dns_config, server_name, server_ip, basedir
+            )
+            log_list.append(sentry_log)
+
     elif len(sys.argv) == 3:
         server_name, server_ip = sys.argv[1:]
-        sentry_log = SentryLog(DNS_DICT, server_name, server_ip)
+        for project, dns_config in DNS_DICT.items():
+            sentry_log = SentryLog(
+                project, dns_config, server_name, server_ip)
+            log_list.append(sentry_log)
     else:
         print "params error"
         print "python xx.py server_name server_ip, basedir"
         sys.exit()
 
-    sentry_log.monitor_start()
+    workers = []
+    for log in log_list:
+        workers.append(gevent.spawn(log.monitor_start))
 
-    signal.signal(signal.SIGKILL, sentry_log.monitor_stop)
+    def stop():
+        for log in log_list:
+            log.monitor_stop()
+
+    signal.signal(signal.SIGKILL, stop)
+
+    gevent.joinall(workers)
 
 if __name__ == "__main__":
 
