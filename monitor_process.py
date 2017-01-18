bda9d4cf3329f54851db0c242968f948cf021fff
guominglei <rickguominglei@hotmail.com>
2017-01-18 11:18:04 +0800

    修改

--- monitor_process.py
+++ monitor_process.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
 """
@@ -13,26 +14,7 @@ import pyinotify
 
 from multiprocessing import Process
 from log_util import FileObject
-
-
-DNS_DICT = {
-
-    "userapi": {
-        "dns": ("http://d609297712ab4017bcc88ee744aa3754:"
-                "2082c2080e4f40f2bd5e8b51049d149b@localhost:9000/2"),
-        "log_path": None
-    },
-    "baseapi": {
-        "dns": ("http://e69dc261db4d4158873d2669308a46c8:"
-                "f8f5d9435fb44f5d91b9f06ddb43fd25@localhost:9000/4"),
-        "log_path": None
-    },
-    "fastapi": {
-        "dns": ("http://88c66f483acb42348ef8ab4e0e3edb03:"
-                "99d91db22b6e410487b214c920ce7a71@localhost:9000/3"),
-        "log_path": "/opt/sports/fastapi/t_logs/t.log",
-    }
-}
+from conf import DNS_DICT
 
 
 class ProcessSingleFile(pyinotify.ProcessEvent):
